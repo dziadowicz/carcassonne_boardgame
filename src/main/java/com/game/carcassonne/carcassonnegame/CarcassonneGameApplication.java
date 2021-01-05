d9c325c2cfc33e6d37f2e2cc92b44034de57e38a
Mateusz Dziadowicz <dziadulus@hotmail.com>
2021-01-05 19:27:43 +0100

    Carcasonne v0.1 Created Repository

---
+++ src/main/java/com/game/carcassonne/carcassonnegame/CarcassonneGameApplication.java
@@ -0,0 +1,13 @@
+package com.game.carcassonne.carcassonnegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarcassonneGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarcassonneGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/game/carcassonne/carcassonnegame/CarcassonneGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.carcassonne.carcassonnegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarcassonneGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
